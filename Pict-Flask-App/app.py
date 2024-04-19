596e28d0b763beb40e93551aedd49d2725180b4a
Yogesh Lale <142746009+beingdas@users.noreply.github.com>
2024-04-20 01:59:50 +0530

    database initial setup

--- Pict-Flask-App/app.py
+++ Pict-Flask-App/app.py
@@ -6,3 +6,7 @@ import assemblyai as aai
 import pymongo
 from bson import json_util
 
+client = pymongo.MongoClient("mongodb+srv://admin:ZdltdZvnbWb0aUk7@cluster0.oygcfbr.mongodb.net/")
+db = client["Pict-Project"]
+collection_Questions = db["Questions"]
+collection_Transcript = db["Transcript"]
